7d613505f6dab0be41dd466c64c4424f54e1fa20
Kelly <119141863+k-leahy@users.noreply.github.com>
2023-01-04 17:22:19 -0700

    Add code

--- arrays.rb
+++ arrays.rb
@@ -1,7 +1,12 @@
 color = ["red", "green", "blue", "brown"]
-
+# This code will make my list and add what I tell it to at the end.
+p color.push("yellow")
 tire = [2, 4, 8, 18]
-
+# This code will only list my first item from the array.
+p tire.shift
 money = [1.99, 2.32, 25.48, 5.99]
-
+# This code will take whatever I insert and put it infront of my list.
+p money.unshift(0.69)
 married = ["true", "false", "false", "false"]
+# This code will only list the last item
+p married.pop
