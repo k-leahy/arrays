14cf91628e9151fe9210e86323949267d540c904
Kelly <119141863+k-leahy@users.noreply.github.com>
2023-01-04 17:33:12 -0700

    Add new method

--- arrays.rb
+++ arrays.rb
@@ -13,3 +13,6 @@ p married.pop
 
 #  My understanding of the idex postion in this file is the first item listed in the array is considered #0
 #  so for 4 items they would be postion [0, 1, 2, 3] or for colors red=0, green=1 blue=2 brown=3
+
+# [].length counts the number of items in the array
+p color.length
